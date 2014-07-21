31c240d1dd8ba298b3972608d56ac46d9e3f8725
Tessa <tessathornton@gmail.com>
2014-07-21 13:11:18 -0400

    debug log

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var Seasons = React.createClass({
         success: function(data) {
           localStorage.setItem('episodes', JSON.stringify(data));
           this.setState({data: data});
+          console.log(data);
         }.bind(this)
       });
     } else {
